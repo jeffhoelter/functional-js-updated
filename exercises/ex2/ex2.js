61dd9de995876d672340d9fd55e85902360e1aea
Jeff Hoelter <jeffh@loar.net>
2018-06-18 16:01:59 -0400

    Completed exercise 2.

--- exercises/ex2/ex2.js
+++ exercises/ex2/ex2.js
@@ -1,27 +1,37 @@
-function output(txt) {
-	console.log(txt);
+function negate(fn) {
+  return function negated(...args) {
+    return !fn(...args);
+  };
 }
 
-function printIf(predicate) {
-	return function(msg) {
-		if (predicate(msg)) {
-			output(msg);
-		}
-	};
+function outputWrapper(fn) {
+  return function outputted(...args) {
+    return fn(...args);
+  };
+}
+
+var output = outputWrapper(console.log.bind(console));
+
+function printIf(fn) {
+  return function(predicate) {
+    return function(...args) {
+      if (predicate(...args)) {
+        return fn(...args);
+      }
+    };
+  };
 }
 
 function isShortEnough(str) {
-	return str.length <= 5;
+  return str.length <= 5;
 }
 
-function isLongEnough(str) {
-	return !isShortEnough(str);
-}
+var isLongEnough = negate(isShortEnough);
 
 var msg1 = "Hello";
 var msg2 = msg1 + " World";
 
-printIf(isShortEnough)(msg1);		// Hello
-printIf(isShortEnough)(msg2);
-printIf(isLongEnough)(msg1);
-printIf(isLongEnough)(msg2);		// Hello World
+printIf(output)(isShortEnough)(msg1); // Hello
+printIf(output)(isShortEnough)(msg2);
+printIf(output)(isLongEnough)(msg1);
+printIf(output)(isLongEnough)(msg2); // Hello World
