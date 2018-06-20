819e689a1ae9e29d35d009d0048b486a3344486a
Jeff Hoelter <jeffh@loar.net>
2018-06-20 11:43:47 -0400

    Completed exercise 4.

--- exercises/ex4/ex4.js
+++ exercises/ex4/ex4.js
@@ -1,13 +1,27 @@
 function lotteryNum() {
-	return (Math.round(Math.random() * 100) % 58) + 1;
+  return Math.round(Math.random() * 100) % 58 + 1;
 }
 
-function pickNumber(){}
+function pickNumber(lotteryNumbers) {
+  var tempLotteryNumbers = lotteryNumbers.slice(0);
+
+  var newLotteryNumber = lotteryNum();
+
+  if (!lotteryNumbers.hasOwnProperty(newLotteryNumber)) {
+    tempLotteryNumbers.unshift(newLotteryNumber);
+    tempLotteryNumbers.sort();
+  }
+
+  return tempLotteryNumbers;
+}
 
 var luckyLotteryNumbers = [];
 
-for (var i = 0; i < 6; i++) {
-	pickNumber();
+for (var i = 0; i < 600; i++) {
+  luckyLotteryNumbers = pickNumber(Object.freeze(luckyLotteryNumbers));
 }
 
 console.log(luckyLotteryNumbers);
+
+var tempArray = [1, 2, 3];
+console.log(tempArray.hasOwnProperty(1));
