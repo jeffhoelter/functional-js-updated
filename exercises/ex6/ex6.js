c4953bf2acc8eb05c9b93cb97c5fb67a1441ce39
Jeff Hoelter <jeffh@loar.net>
2018-06-21 15:59:29 -0400

    Completed exercise 6.

--- exercises/ex6/ex6.js
+++ exercises/ex6/ex6.js
@@ -1,7 +1,14 @@
-function mult(x,y,z) {
-	return x * y * z;
+function mult(x, y, z) {
+  return x * y * z;
 }
 
-mult(3,4,5);	// 60
+function multR(product, ...rest) {
+  if (rest.length === 0) {
+    return product;
+  }
+  return product * multR(...rest);
+}
 
-mult(3,4,5,6);	// Oops!
+console.log(multR(3, 4, 5)); // 60
+
+console.log(multR(3, 4, 5, 6)); // Oops!
